bec750455cbc65fa86410bde278dc496fbda482b
wangyang <18518317472@163.com>
2021-02-05 10:51:28 +0800

    bug

--- SSO.Util.Client4.5/SSO.Util.Client/TcpListenerHelper.cs
+++ SSO.Util.Client4.5/SSO.Util.Client/TcpListenerHelper.cs
@@ -8,15 +8,27 @@ using System.Threading.Tasks;
 
 namespace SSO.Util.Client
 {
+    /// <summary>
+    /// tcp监听辅助类
+    /// </summary>
     public class TcpListenerHelper
     {
         int port = 0;
         int buffer = 256;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="port">要监听的端口</param>
+        /// <param name="buffer">缓存的字节大小</param>
         public TcpListenerHelper(int port, int buffer = 256)
         {
             this.port = port;
             this.buffer = buffer;
         }
+        /// <summary>
+        /// 运行监视
+        /// </summary>
+        /// <param name="action">监视回调</param>
         public void Run(Action<string> action)
         {
             IPAddress localAddr = IPAddress.Parse(GetLocalIp());
@@ -56,6 +68,10 @@ namespace SSO.Util.Client
                 server.Stop();
             }
         }
+        /// <summary>
+        /// 获取计算机的ip
+        /// </summary>
+        /// <returns></returns>
         public string GetLocalIp()
         {
             ///获取本地的IP地址
