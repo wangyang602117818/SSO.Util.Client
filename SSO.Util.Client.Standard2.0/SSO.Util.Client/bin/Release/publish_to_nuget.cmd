80e6ee30b16def55427dcd36b3ca21f3bf4329c3
wanghome <602117818@qq.com>
2022-12-18 17:03:02 +0800

    更新

--- SSO.Util.Client.Standard2.0/SSO.Util.Client/bin/Release/publish_to_nuget.cmd
+++
@@ -1,6 +0,0 @@
-set input=
-set /p input=please input version number:
-echo publish: %input%
-dotnet nuget push SSO.Util.Client.%input%.nupkg -k oy2jfmh7vxclkuln4izqec6zvw22wnlulh5d2nkudq22gy -s https://api.nuget.org/v3/index.json
-pause
-goto begin
